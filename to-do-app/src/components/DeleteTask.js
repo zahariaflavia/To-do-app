a00daf0696886cffe0e06fabfab8fa7fb6de2492
zahariaflavia <flavia.zaharia@3pillarglobal.com>
2021-07-27 17:32:12 +0300

    display completed and incompleted tasks

--- to-do-app/src/components/DeleteTask.js
+++ to-do-app/src/components/DeleteTask.js
@@ -1,45 +1,34 @@
-import { useState,useEffect } from "react";
-const DeleteTask=()=>{
-    const data=JSON.parse(localStorage.getItem("tasks"))||[];
-    //console.log(data);
-    const [tasks,setTasks]=useState(data);
-    // useEffect(()=>{
-    //     const data=JSON.parse(localStorage.getItem("tasks"));
-    //     setTasks(data);
-    // },[])
+    import { useState,useEffect } from "react";
+    const DeleteTask=()=>{
+        const data=JSON.parse(localStorage.getItem("tasks"))||[];
+        //console.log(data);
+        const [tasks,setTasks]=useState(data);
+        // useEffect(()=>{
+        //     const data=JSON.parse(localStorage.getItem("tasks"));
+        //     setTasks(data);
+        // },[])
 
-    const removeTask=(id)=>{
-        let aux=tasks.filter((task)=>task.id!==id)
-        setTasks(aux);
-        localStorage.setItem("tasks",JSON.stringify(aux));
-        
+        const removeTask=(id)=>{
+            let aux=tasks.filter((task)=>task.id!==id)
+            setTasks(aux);
+            localStorage.setItem("tasks",JSON.stringify(aux));
+            
+        }
+        return(<div className="items">
+            {tasks.length===0?(<h2>There are no activities to pe displayed</h2>):( <table className="table">
+        {tasks.map((task)=>{
+
+            const {id,name}=task;
+            return (
+                <tr>
+                    <td>{name}</td>
+                    <td> <button  className="button" onClick={()=>removeTask(id)}>Delete</button></td>
+                </tr>
+            );
+        })}
+        </table>)}
+           
+        </div>);
+
     }
-    return(<div className="items">
-        {tasks.length===0?(<h2>There are no activities to pe displayed</h2>):( <table className="table">
-      {tasks.map((task)=>{
-
-          const {id,name}=task;
-          return (
-              <tr>
-                  <td>{name}</td>
-                  <td> <button  className="button" onClick={()=>removeTask(id)}>Delete</button></td>
-              </tr>
-          );
-      })}
-      </table>)}
-        {/* <table className="table">
-      {tasks.map((task)=>{
-
-          const {id,name}=task;
-          return (
-              <tr>
-                  <td>{name}</td>
-                  <td> <button  className="button" onClick={()=>removeTask(id)}>Delete</button></td>
-              </tr>
-          );
-      })}
-      </table> */}
-    </div>);
-
-}
-export default DeleteTask;
+    export default DeleteTask;
--- to-do-app/src/components/DisplayActivities.js
+++ to-do-app/src/components/DisplayActivities.js
@@ -1,31 +1,42 @@
-import {useState} from 'react'
-const DisplayActivities=()=>{
-    const data=JSON.parse(localStorage.getItem("tasks"))||[];
-    const [tasks,setTasks]=useState(data);
-    const handleChange=(id)=>{
-       const aux=[...tasks];
-       const elementIndex=tasks.findIndex(element=>element.id===id)
-       aux[elementIndex]={...aux[elementIndex],status:"complete"};
-       setTasks(aux);
-       window.localStorage.setItem("tasks",JSON.stringify(aux));
+    import {useState} from 'react'
+    import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+    const DisplayActivities=()=>{
+        const data=JSON.parse(localStorage.getItem("tasks"))||[];
+        const [tasks,setTasks]=useState(data);
+        const handleChange=(id)=>{
+        const aux=[...tasks];
+        const elementIndex=tasks.findIndex(element=>element.id===id)
+        aux[elementIndex]={...aux[elementIndex],status:"complete"};
+        setTasks(aux);
+        window.localStorage.setItem("tasks",JSON.stringify(aux));
+        }
+        return <div className="items">
+            {tasks.filter((task)=>task.status==='incomplete').length===0?(<h2>Congratulations! You completed all your activities!</h2>):(<h3>Activities to be completed: {tasks.filter((task)=>task.status==='incomplete').length}</h3>)}
+            <table className="table">
+                <tbody>
+        {tasks.filter((task)=>task.status==='incomplete').map((task)=>{
+            const {id,name,status}=task;
+            return(
+                <tr key={id}>
+                <td>{name}</td>
+                <td><button className="button" onClick={()=>handleChange(id)}>Done</button></td>
+                </tr>
+                
+            );
+        })}
+        {tasks.filter((task)=>task.status==='complete').map((task)=>{
+            const {id,name,status}=task;
+            return(
+                <tr key={id}>
+                <td>{name}</td>
+                <td><CheckCircleOutlineIcon/></td>
+                </tr>
+                
+            );
+        })}
+        </tbody>
+        </table> 
+        </div>
     }
-    return <div className="items">
-        <table className="table">
-            <tbody>
-    {tasks.map((task)=>{
-        const {id,name,status}=task;
-        return(
-            <tr key={id}>
-            <td>{name}</td>
-            <td>{status}</td>
-            <td><button className="button" onClick={()=>handleChange(id)}>Done</button></td>
-            </tr>
-            
-        );
-    })}
-    </tbody>
-    </table>
-    </div>
-}
 
-export default DisplayActivities
+    export default DisplayActivities
